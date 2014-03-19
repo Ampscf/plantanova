bc63cba6318a008f9036bafda20c65b69ed1a909
Lord-AlexG <alexgh312@gmail.com>
2014-03-19 11:01:37 -0600

    Se cambia a español la vista de Login

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -1,4 +1,3 @@
-
 <div class="page-container">
 	<div class="row">
 		<div class="logo">
@@ -10,7 +9,7 @@
 
 			<div class="panel panel-default">
 				<div class="panel-heading">
-					<h3 class="panel-title"> Log in </h3>
+					<h3 class="panel-title"> Acceder </h3>
 				</div>
 
 				<div class="panel-body" style="padding: 10px 10px 10px 10px;">
@@ -18,7 +17,7 @@
 						<div class="input-group-addon">
 							<span class="glyphicon glyphicon-user"></span>
 						</div>
-                    	<input type="text" class="form-control" placeholder="Username" name="username" id="username">
+                    	<input type="text" class="form-control" placeholder="Correo" name="username" id="username">
                 	</div>
 
                 	<div class="clear"></div>
@@ -27,17 +26,17 @@
 						<div class="input-group-addon">
 							<span class="glyphicon glyphicon-eye-close"></span>
 						</div>
-                    	<input type="password" class="form-control" placeholder="Password" name="password" id="password">
+                    	<input type="password" class="form-control" placeholder="Contraseña" name="password" id="password">
                 	</div>
 				</div>
 
 				<div class="panel-footer">
 					<div class="row">
 						<div class="col-md-2">
-							<button href="#" class="btn btn-link">Forgot password?</button>
+							<button href="#" class="btn btn-link">Recuperar contraseña</button>
 						</div>
 						<div class="col-md-3 col-md-offset-6">
-							<button class="btn btn-success btn-block">Login</button>
+							<button class="btn btn-success btn-block">Acceder</button>
 						</div>
 					</div>
 				</div>
