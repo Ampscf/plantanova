61a2953e220d7d47d8ef92b6f2e786ceae2e456b
criss00zx <criss00zx@gmail.com>
2014-03-19 13:34:34 -0600

    el login en php

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -12,34 +12,36 @@
 					<h3 class="panel-title"> Acceder </h3>
 				</div>
 
-				<div class="panel-body" style="padding: 10px 10px 10px 10px;">
-					<div class="input-group input-group-lg">
-						<div class="input-group-addon">
-							<span class="glyphicon glyphicon-user"></span>
-						</div>
-                    	<input type="text" class="form-control" placeholder="Correo" name="username" id="username">
-                	</div>
+				<?php				
+				echo "<div class='panel-body' style='padding: 10px 10px 10px 10px;'>";
+					echo "<div class='input-group input-group-lg'>";
+						echo "<div class='input-group-addon'>";
+							echo "<span class='glyphicon glyphicon-user'></span>";
+						echo "</div>";
+                    	echo "<input type='text' class='form-control' placeholder='Correo' name='correo' id='correo'>";
+                	echo "</div>";
 
-                	<div class="clear"></div>
+                	echo "<div class='clear'></div>";
 
-					<div class="input-group input-group-lg">
-						<div class="input-group-addon">
-							<span class="glyphicon glyphicon-eye-close"></span>
-						</div>
-                    	<input type="password" class="form-control" placeholder="Contraseña" name="password" id="password">
-                	</div>
-				</div>
+					echo "<div class='input-group input-group-lg'>";
+						echo "<div class='input-group-addon'>";
+							echo "<span class='glyphicon glyphicon-eye-close'></span>";
+						echo "</div>";
+                    	echo "<input type='password' class='form-control' placeholder='Contraseña' name='contraseña' id='contraseña'>";
+                	echo "</div>";
+				echo "</div>";
 
-				<div class="panel-footer">
-					<div class="row">
-						<div class="col-md-2">
-							<button href="#" class="btn btn-link">Recuperar contraseña</button>
-						</div>
-						<div class="col-md-3 col-md-offset-6">
-							<button class="btn btn-success btn-block">Acceder</button>
-						</div>
-					</div>
-				</div>
+				echo "<div class='panel-footer'>";
+					echo "<div class='row'>";
+						echo "<div class='col-md-2'>";
+							echo "<button href='#' class='btn btn-link'>Recuperar contraseña</button>";
+						echo "</div>";
+						echo "<div class='col-md-3 col-md-offset-6'>";
+							echo "<button class='btn btn-success btn-block'>Acceder</button>";
+						echo "</div>";
+					echo "</div>";
+				echo "</div>";
+				?>
 			</div>
 
 		</div>
