811e0759bd0b4bc3974ee116cd87f8eb2a61bdf0
Lord-AlexG <alexgh312@gmail.com>
2014-03-21 14:49:11 -0600

    commit

--- application/controllers/Login_controller.php
+++ application/controllers/Login_controller.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Login_controller extends CI_Controller {
+class login_controller extends CI_Controller {
 
 	function __construct() {
 	   parent::__construct();
