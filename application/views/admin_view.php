ca6395fcff6857e2aa1937ceb8f68e07db03e5df
criss00zx <criss00zx@gmail.com>
2014-03-25 09:15:50 -0600

    se agregan pantallas del admin

---
+++ application/views/admin_view.php
@@ -0,0 +1,40 @@
+<div align="center">
+<h2>Pedidos en tránsito</h2>
+<table border="1" class="table">
+	<tr>
+		<td><b> #Pedido </b></td>
+		<td><b> Fecha de entrega </b></td>
+		<td><b> Planta </b></td> 
+		<td><b> Categoría  </b></td>
+		<td><b> Cliente  </b></td>
+	</tr>
+	<tr>
+		<td><?php echo anchor('Order_controller/pedido1/', '1');?></td>
+		<td>05/05/2014</td>
+		<td>Sandía</td>
+		<td>Orden</td>
+		<td>Jorge Torres</td>
+    </tr>
+	<tr>
+		<td><?php echo anchor('Order_controller/index/', '2');?></td>
+		<td>10/12/2014</td>
+		<td>Tomate</td>
+		<td>Orden</td>
+		<td>Jorge Torres</td>
+    </tr>	
+	<tr>
+		<td><?php echo anchor('Order_controller/index/', '3');?></td>
+		<td>09/08/2014</td>
+		<td>Tomate</td>
+		<td>Orden</td>
+		<td>Jorge Torres</td>
+    </tr>
+	<tr>
+		<td><?php echo anchor('Order_controller/index/', '4');?></td>
+		<td>21/06/2014</td>
+		<td>Tomate</td>
+		<td>Orden</td>
+		<td>Jorge Torres</td>
+    </tr>
+</table>
+</div>
---
+++ application/views/pedido1.php
@@ -0,0 +1,325 @@
+<div class="container">
+	<div class=" col-md-10 col-md-offset-1">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+	    		<h3 class="panel-title">Información del cliente</h3>
+	  		</div>
+			<div class="panel-body">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>
+								Nombre
+							</th>
+							<th>
+								Agricola
+							</th>	
+							<th>
+								Correo electrónico
+							</th>
+							<th>
+								Teléfono
+							</th>
+							<th>
+								Dirección
+							</th>
+							<th>
+								Ciudad
+							</th>
+							<th>
+								Estado
+							</th>
+							<th>
+								Código postal
+							</th>
+							<th>
+								RFC
+							</th>
+						</tr>	
+					</thead>
+					<tbody>
+						<tr>
+							<td>Jorge Torres</td>
+							<td>Agricultores de Querétaro</td>
+							<td>cliente@cliente.com</td>
+							<td>4425634123</td>
+							<td>Av. Constituyentes #5</td>
+							<td>Querétaro</td>
+							<td>Querétaro</td>
+							<td>78128</td>
+							<td>ADQ910312OL8</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="container">
+	<div class=" col-md-10 col-md-offset-1">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+	    		<h3 class="panel-title">Información del pedido</h3>
+	  		</div>
+			<div class="panel-body">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>
+								# de pedido
+							</th>
+							<th>
+								Planta
+							</th>	
+							<th>
+								Fecha de entrega
+							</th>
+							<th>
+								Variedad
+							</th>
+							<th>
+								Porta Injerto
+							</th>
+							<th>
+								Brazos
+							</th>
+							<th>
+								Vol. total
+							</th>
+							<th>
+								Tipo sustrato
+							</th>
+							<th>
+								Subtipo
+							</th>
+						</tr>	
+					</thead>
+					<tbody>
+						<tr>
+							<td>1</td>
+							<td>Sandía</td>
+							<td>05/05/2014</td>
+							<td>PaiPai</td>
+							<td>Empower</td>
+							<td>1</td>
+							<td>20000</td>
+							<td>Peat moss</td>
+							<td>Charola 35 cavidades</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="page-container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title"> Siembra </h3>
+				</div>
+	
+				<?php echo form_open('Login_controller/home'); ?>
+					<div class="panel-body" style="padding: 10px 10px 10px 10px;">
+						
+						<div class="input-group-lg">
+							<input type="text" class="form-control" placeholder="Cantidad" name="variedad" id="variedad">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">						
+							<input type="text" class="form-control" placeholder="Fecha" name="portainjerto" id="portainjerto">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">	
+							<input type="text" class="form-control" placeholder="Alcance" name="volumen" id="volumen">
+						</div>
+						
+					</div>
+
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-md-3">
+								<input class="btn btn-default btn-block" type="submit" value="Aceptar"/>
+							</div>
+							<div class="col-md-3 col-md-offset-6">
+								<input class="btn btn-default btn-block" type="button" value="Cancelar"/>
+							</div>
+						</div>
+					</div>
+				</form>
+				
+			</div>
+			<!-- End panel-default -->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title"> Germinación </h3>
+				</div>
+	
+				<?php echo form_open('Login_controller/home'); ?>
+					<div class="panel-body" style="padding: 10px 10px 10px 10px;">
+						
+						<div class="input-group-lg">
+							<input type="text" class="form-control" placeholder="Cantidad" name="variedad" id="variedad">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">						
+							<input type="text" class="form-control" placeholder="Fecha" name="portainjerto" id="portainjerto">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">	
+							<input type="text" class="form-control" placeholder="Alcance" name="volumen" id="volumen">
+						</div>
+						
+					</div>
+
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-md-3">
+								<input class="btn btn-default btn-block" type="submit" value="Aceptar"/>
+							</div>
+							<div class="col-md-3 col-md-offset-6">
+								<input class="btn btn-default btn-block" type="button" value="Cancelar"/>
+							</div>
+						</div>
+					</div>
+				</form>
+				
+			</div>
+			<!-- End panel-default -->
+		<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title"> Injerto </h3>
+				</div>
+	
+				<?php echo form_open('Login_controller/home'); ?>
+					<div class="panel-body" style="padding: 10px 10px 10px 10px;">
+						
+						<div class="input-group-lg">
+							<input type="text" class="form-control" placeholder="Cantidad" name="variedad" id="variedad">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">						
+							<input type="text" class="form-control" placeholder="Fecha" name="portainjerto" id="portainjerto">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">	
+							<input type="text" class="form-control" placeholder="Alcance" name="volumen" id="volumen">
+						</div>
+						
+					</div>
+
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-md-3">
+								<input class="btn btn-default btn-block" type="submit" value="Aceptar"/>
+							</div>
+							<div class="col-md-3 col-md-offset-6">
+								<input class="btn btn-default btn-block" type="button" value="Cancelar"/>
+							</div>
+						</div>
+					</div>
+				</form>
+				
+			</div>
+			<!-- End panel-default -->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title"> Plantado </h3>
+				</div>
+	
+				<?php echo form_open('Login_controller/home'); ?>
+					<div class="panel-body" style="padding: 10px 10px 10px 10px;">
+						
+						<div class="input-group-lg">
+							<input type="text" class="form-control" placeholder="Cantidad" name="variedad" id="variedad">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">						
+							<input type="text" class="form-control" placeholder="Fecha" name="portainjerto" id="portainjerto">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">	
+							<input type="text" class="form-control" placeholder="Alcance" name="volumen" id="volumen">
+						</div>
+						
+					</div>
+
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-md-3">
+								<input class="btn btn-default btn-block" type="submit" value="Aceptar"/>
+							</div>
+							<div class="col-md-3 col-md-offset-6">
+								<input class="btn btn-default btn-block" type="button" value="Cancelar"/>
+							</div>
+						</div>
+					</div>
+				</form>
+				
+			</div>
+			<!-- End panel-default -->
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h3 class="panel-title"> Transplante </h3>
+				</div>
+	
+				<?php echo form_open('Login_controller/home'); ?>
+					<div class="panel-body" style="padding: 10px 10px 10px 10px;">
+						
+						<div class="input-group-lg">
+							<input type="text" class="form-control" placeholder="Cantidad" name="variedad" id="variedad">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">						
+							<input type="text" class="form-control" placeholder="Fecha" name="portainjerto" id="portainjerto">
+						</div>
+						
+						<div class="clear"></div>
+
+						<div class="input-group-lg">	
+							<input type="text" class="form-control" placeholder="Alcance" name="volumen" id="volumen">
+						</div>
+						
+					</div>
+
+					<div class="panel-footer">
+						<div class="row">
+							<div class="col-md-3">
+								<input class="btn btn-default btn-block" type="submit" value="Aceptar"/>
+							</div>
+							<div class="col-md-3 col-md-offset-6">
+								<input class="btn btn-default btn-block" type="button" value="Cancelar"/>
+							</div>
+						</div>
+					</div>
+				</form>
+				
+			</div>
+			<!-- End panel-default -->
+		</div>		
+		<!-- End col-md-4 col-md-offset-4 -->
+	</div>
+	<!-- End row -->
+</div>
+<!-- End page-container -->
