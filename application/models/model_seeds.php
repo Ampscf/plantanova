1f688384b2300fcc47990c22c11853fe93dec26f
criss00zx <criss00zx@gmail.com>
2014-06-06 13:57:48 -0500

    se agrega la tabla de formulario de semillas y ademas ya se pueden eliminar

--- application/models/model_seeds.php
+++ application/models/model_seeds.php
@@ -2,5 +2,29 @@
 
 Class model_seeds extends CI_Model	
 {
-
+	function get_seeds_lists()
+	{
+	
+		$result = $this->db->query('select id_seed, t_seeds.id_order, seed_name, batch, volume, seeds_date, type, farm_name
+									from `t_seeds`, `t_order`, `t_user`
+									where t_seeds.id_order = t_order.id_order and t_order.id_client=t_user.id_user
+									order by id_seed desc');
+											
+		if($result->num_rows() > 0) 
+		{
+			return $result->result();
+		} 
+		else 
+		{
+			return null;
+		} 
+	}
+	
+	function delete_seeds($id)
+	{
+		$this->db->where('id_seed',$id);
+		$this->db->delete('t_seeds');	
+	}
 }
+
+?>
---
+++ application/views/body/view_seeds.php
@@ -0,0 +1,15 @@
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="table-responsive">
+							<?php echo anchor('admin/register_client_form','Agregar Nuevo', 'class="btn btn-success"'); ?>
+							<?php include_once('application/views/extra/tabla_semillas.php'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> <!-- End row -->
+	</div> <!-- End container -->
+</div> <!-- End content div -->
