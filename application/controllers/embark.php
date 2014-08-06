f081c0e3e5411b1e39e3b30a17ad5259c2f76228
criss00zx <criss00zx@gmail.com>
2014-08-06 10:15:02 -0500

    ..

--- application/controllers/embark.php
+++ application/controllers/embark.php
@@ -298,5 +298,26 @@ class Embark extends CI_Controller {
 		}
 	}
 
+	function embark_orders()
+	{
+		//Order resume
+		$template['id_order']=$this->uri->segment(3);
+		$order=$this->model_order->get_order_id_order($this->uri->segment(3));
+		$template['fecha']=$order->result()[0]->order_date_submit;
+		$template['fecha_entrega']=$order->result()[0]->order_date_delivery;
+		$template['planta']=$this->model_order->get_plant($order->result()[0]->id_plant);
+		$template['volumen']=$order->result()[0]->total_volume;
+		$template['categoria']=$this->model_order->get_category($order->result()[0]->id_category);
+		$template['client']=$this->model_user->obtenerCliente($order->result()[0]->id_client);
+		$template['farmer']=$order->result()[0]->farmer;
+
+		//Load template
+		$template['header'] = 'header/view_admin_header.php';
+		$template['body'] = 'body/view_embark_orders.php';
+		$template['footer'] = 'footer/view_footer.php';
+
+		$this->load->view("main",$template);
+	}
+
 
 }
