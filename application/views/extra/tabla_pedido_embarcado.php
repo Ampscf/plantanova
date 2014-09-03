cae7057d363378b31884b9ea6d218f8fca067a89
criss00zx <criss00zx@gmail.com>
2014-09-03 13:23:19 -0500

    arreglados algunos asuntos visuales

--- application/views/extra/tabla_pedido_embarcado.php
+++ application/views/extra/tabla_pedido_embarcado.php
@@ -76,7 +76,7 @@
 	            				<div class="modal-content">
 	                				<div class="modal-header">
 	                    				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-	                    				<h4 class="modal-title"></h4>
+	                    				<h4 class="modal-title">Modificar pedido</h4>
 	                				</div>
 	                				<div class="modal-body">
 	                    				<p>¿Seguro que deseas editar este pedido?</p>
@@ -96,7 +96,7 @@
 	            				<div class="modal-content">
 	                				<div class="modal-header">
 	                    				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-	                    				<h4 class="modal-title"></h4>
+	                    				<h4 class="modal-title">Modificar embarque</h4>
 	                				</div>
 	                				<div class="modal-body">
 	                    				<p>¿Seguro que deseas editar la información del embarque?</p>
@@ -116,7 +116,7 @@
 	            				<div class="modal-content">
 	                				<div class="modal-header">
 	                    				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-	                    				<h4 class="modal-title"></h4>
+	                    				<h4 class="modal-title">Finalizar pedido</h4>
 	                				</div>
 	                				<div class="modal-body">
 	                    				<p>¿Seguro que deseas finalizar este pedido?</p>
