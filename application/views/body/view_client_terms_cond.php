06731fecec55207c487da83aa4d75f45dc78e8cd
iscghg <gustavo@ektroid.com>
2014-09-22 18:02:04 -0500

    se agrga la vista de terminos y condiciones

---
+++ application/views/body/view_client_terms_cond.php
@@ -0,0 +1,20 @@
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="panel panel-default">
+				
+					<div class="panel-body">
+						<div data-spy="scroll" data-target=".navbar-example" style="position: relative;">
+							<p>aqui van los terminos y condiciones</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div> <!-- End row -->	
+	</div> <!-- End container -->	
+</div> <!-- End content div -->
+
+<script>
+$('div').scrollspy({ target: '.navbar-example' })
+</script>
