3d97be58e28a04acf55c1ba7497c096e635e5bd1
iscghg <gustavo@ektroid.com>
2014-10-07 12:03:29 -0500

    se hacen pruebas para la descarga del excel

--- application/controllers/reports.php
+++ application/controllers/reports.php
@@ -8,6 +8,7 @@ class Reports extends CI_Controller {
 		$this->load->library('Excel');
 		$this->load->model('model_user','',TRUE);
 		$this->load->model('model_report','',TRUE);
+		$this->load->helper('download');
 
 	}
 	public function report()
@@ -30,48 +31,57 @@ class Reports extends CI_Controller {
 		$users= $this->model_report->get_users();
 		$cont=1;
 		foreach($users as $key) {
-		$orders=$this->model_report->get_orders($key->id_user);
-		if($orders){
-			$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,$cont,"Cliente: ". $key->first_name); 
-			$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4,$cont,"Siembra");
-			$cont++;
-			foreach($orders as $key2) {
-				$breakdowns=$this->model_report->get_breakdown($key2->id_order);
-				if($breakdowns){
-					foreach($breakdowns as $key3){
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,$cont,"PEDIDO");  
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,$cont,"FECHA");  
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4,$cont,"Cantidad"); 
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5,$cont,"Fecha"); 
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6,$cont,"Alcance");  
-						$cont++;
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,$cont,"VARIEDAD");
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1,$cont,$key3->variety);
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,$cont,$key3->volume);  
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,$cont,$key2->order_date_submit);
-						$cont++;
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,$cont,"PORTAINJERTO");
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1,$cont,$key3->rootstock);  
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,$cont,$key3->volume);  
-						$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,$cont,$key2->order_date_submit);
-						$cont++;
-					}
+			$orders=$this->model_report->get_orders($key->id_user);
+			if($orders){
+				$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,$cont,"Cliente: ". $key->first_name); 
+				$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4,$cont,"Siembra");
+				$cont++;
+				foreach($orders as $key2) {
+					$breakdowns=$this->model_report->get_breakdown($key2->id_order);
+					if($breakdowns){
+						foreach($breakdowns as $key3){
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,$cont,"PEDIDO");  
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,$cont,"FECHA");  
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4,$cont,"Cantidad"); 
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5,$cont,"Fecha"); 
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6,$cont,"Alcance");  
+							$cont++;
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,$cont,"VARIEDAD");
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1,$cont,$key3->variety);
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,$cont,$key3->volume);  
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,$cont,$key2->order_date_submit);
+							$cont++;
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(0,$cont,"PORTAINJERTO");
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1,$cont,$key3->rootstock);  
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2,$cont,$key3->volume);  
+							$objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3,$cont,$key2->order_date_submit);
+							$cont++;
+						}
 
+					}
 				}
+			
 			}
-		
 		}
-	
-	
-	}
 
 		$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
 		$fichero="./reportes/reporte_plantanova.xlsx";
 		$objWriter->save($fichero);
+
+		$data = file_get_contents("./reportes/reporte_plantanova.xlsx"); // Read the file's contents
+		$name = 'reporte_plantanova.xlsx';
+		force_download($name, $data);
+	}
+
+	public function report_body(){
+		
+
 		$template['header'] = 'header/view_admin_header.php';
 		$template['body'] = 'body/view_admin_report.php';
-		$template['footer'] = "footer/view_footer.php";		
+		$template['footer'] = "footer/view_footer.php";	
 		$this->load->view('main',$template);
+
+
 	}
 
 
